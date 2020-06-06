1edeecae80a24df499289eeb6a75452798e917c5
Jhon Mark <villegas_jhon_mark@ring-group.jp>
2020-06-06 21:02:39 +0800

    refs #00010 - adding bold text in billing show page

--- resources/views/billing/show.blade.php
+++ resources/views/billing/show.blade.php
@@ -13,7 +13,7 @@
             <div class="col-lg-6">
                 <div class="card card-success card-outline">
                     <div class="card-header">
-                        <h3 class="card-title text-uppercase">Details</h3>
+                        <h3 class="card-title text-uppercase font-weight-bold">Details</h3>
                         <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                         </div>
@@ -21,20 +21,20 @@
                     <div class="card-body pt-2 pb-0">
                         <ul class="list-group list-group-unbordered mb-3">
                             <li class="list-group-item border-top-0">
-                                <b>Client Name</b> 
+                                <span class="text-uppercase font-weight-bold">Client Name</span>
                                 <span class="float-right">
                                     {{ $billing->client->name }}
                                 </span>
                             </li>
                             <li class="list-group-item">
-                                <b>Date Created</b> 
+                                <span class="text-uppercase font-weight-bold">Date Created</span> 
                                 <span class="float-right">
                                     {{ $billing->created_at }}
                                 </span>
                             </li>
                             <li class="list-group-item border-bottom-0">
                                 <div class="border border-top-0 border-left-0 border-right-0 pb-2">
-                                   <span class="font-weight-bold d-inline-block mb-2"> Description</span>
+                                   <span class="d-inline-block mb-2 text-uppercase font-weight-bold"> Description</span>
                                     <p class="text-justify" style="text-indent: 5%">
                                         {!! $billing->description !!}
                                     </p>
@@ -49,7 +49,7 @@
             <div class="col-lg-12">
                 <div class="card card-success card-outline col-md-12 pl-0 pr-0">
                     <div class="card-header">
-                        <h3 class="card-title text-uppercase">Billing Items</h3>
+                        <h3 class="card-title text-uppercase font-weight-bold">Billing Items</h3>
                         <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                         </div>
