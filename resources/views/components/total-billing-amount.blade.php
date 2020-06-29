7258f47340b674f8c5625578379d90d90e6334a3
Jhon Mark <villegas_jhon_mark@ring-group.jp>
2020-06-30 03:28:59 +0800

    refs 00034 - adding a payment modal in billing index page

--- resources/views/components/total-billing-amount.blade.php
+++ resources/views/components/total-billing-amount.blade.php
@@ -1,6 +1,6 @@
-<div class="col-md-6 text-right">
-    <p class="h1 font-weight-bold text-uppercase">Total Amount</p>
-    <p class="h3 font-weight">
+<div {{ $attributes->merge(['class' => 'col-md-6 text-right']) }}>
+    <p class="{{ isset($paymentUpdate) ? 'h3' : 'h1'}} font-weight-bold text-uppercase">Total {{ $paymentUpdate ?? 'Amount'}}</p>
+    <p class="{{ isset($paymentUpdate) ? 'h6' : 'h3' }}  font-weight">
         <span class="d-inline-block ml-1">&#x20B1;</span>
         <span class="font-weight-light" id="total-amount-value">{{ number_format($totalAmount, 2) }} </span>
     </p>
